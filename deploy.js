a4b761c128a33f9f6b17a137ac0623516fb22962
Nya Candy <dev@candinya.com>
2023-02-07 10:26:59 +0800

    chore: lint

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 import fetch, { FormData, fileFromSync } from "node-fetch";
-import fs from 'fs';
+import fs from "fs";
 
 const distFile = "dist/index.css";
 
@@ -7,18 +7,21 @@ const distFile = "dist/index.css";
 if (!fs.existsSync(distFile)) {
   console.error("Target file doesn't exist, please build it first.");
 } else {
-
   const formData = new FormData();
-  formData.append('file', fileFromSync(distFile));
+  formData.append("file", fileFromSync(distFile));
 
-  fetch('https://ipfs-relay.crossbell.io/upload', {
-    method: 'POST',
+  fetch("https://ipfs-relay.crossbell.io/upload", {
+    method: "POST",
     body: formData,
-  }).then(r => r.json()).then(r => {
-    console.log("Your theme has been uploaded successfully, please add following line to your xLog custom css: ")
-    console.log(`@import url("${r.url}");`)
-  }).catch(e => {
-    console.error("Upload failed with error: ", e)
   })
-
+    .then((r) => r.json())
+    .then((r) => {
+      console.log(
+        "Your theme has been uploaded successfully, please add following line to your xLog custom css: "
+      );
+      console.log(`@import url("${r.url}");`);
+    })
+    .catch((e) => {
+      console.error("Upload failed with error: ", e);
+    });
 }
